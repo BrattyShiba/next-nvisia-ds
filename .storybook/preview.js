a72e113cc7af55398e601fdc6e71620eea86c4de
Kathrin Rohrmeier <theliteratecondition@gmail.com>
2022-11-18 15:36:10 -0600

    [base components] attempts to incl. style in storybook
    - do we need to track the storybook static files??
    - https://stackoverflow.com/questions/63498393/storybook-component-level-css-file-not-working
    - https://storybook.js.org/docs/react/configure/overview#configure-story-rendering
    - https://nextjs.org/docs/basic-features/built-in-css-support#adding-component-level-css
    I attempted the above, plus other potential solutions) but was unsuccessful (though, I may have mistyped something)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+// import React from "react"; // needed for context if using Styled Components
+/**
+ * install storybook-addon-next & "If you have global imported styles, create a file called .storybook/preview.js and import the styles there. They will be added by Storybook automatically for all stories." https://storybook.js.org/docs/react/get-started/setup#render-component-styles
+ * Lies.
+ */
+// import '../src/styles/global.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +14,13 @@ export const parameters = {
     },
   },
 }
+/**
+ * to provide the theme if using a context for Styled Components (do we want to use Styled COmponents?)
+ */
+// export const decorators = [
+//   (Story) => (
+//     <ThemeProvider theme="default">
+//       <Story />
+//     </ThemeProvider>
+//   ),
+// ];
