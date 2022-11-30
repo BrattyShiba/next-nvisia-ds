3bee97df38ea57ee4a122ca4fa4246cd1aa8eeb0
Kathrin Rohrmeier <theliteratecondition@gmail.com>
2022-11-30 11:13:10 -0600

    [accordion] clean-up in utilities & InputField

--- src/utilities.js
+++ src/utilities.js
@@ -1,10 +1,8 @@
 export function toTitleCase(words) {
-  console.log('words to title case: ', words);
-  
   // lowercase the param string and split on each word (space)
   return words.toLowerCase().split(" ").map((word) => {
       // uppercase the first letter & append to the rest of the word
       return (word.charAt(0).toUpperCase() + word.slice(1));
     }
   ).join(" "); // rejoin what we split above before the mapping
-}
+};
