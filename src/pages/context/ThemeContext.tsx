470b666cbd17f0a01863b85585349f07261ce6f4
BrattyShiba <oliverramirezv@gmail.com>
2023-03-20 20:00:03 -0500

    removing posting events to George's API

--- src/pages/context/ThemeContext.tsx
+++ src/pages/context/ThemeContext.tsx
@@ -27,15 +27,6 @@ const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
       message: `Lights ${themeStatus} over at the Design System`,
       json: "{}",
     });
-    // fetch(`${process.env.API}`, {
-    //   method: "POST",
-    //   mode: "cors",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Authorization: `${process.env.APIKEY}`,
-    //   },
-    //   body: bodyRequest,
-    // });
   };
 
   return (
